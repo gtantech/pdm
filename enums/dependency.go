87cc404b9ba40b9147263a3219860b5689d48cc4
gtantech <tangabrielkl@gmail.com>
2026-08-30 17:57:44 -0400

    Moved Dependency into separate package

--- enums/dependency.go
+++ enums/dependency.go
@@ -8,19 +8,3 @@ const (
 	SF DependencyType = "SF" // start to finish relationship
 	FF DependencyType = "FF" //finish to finish relationship
 )
-
-type Dependency interface {
-	Type() DependencyType
-}
-
-type relationship struct {
-	kind DependencyType
-}
-
-func New(kind DependencyType) *relationship {
-	return &relationship{kind: kind}
-}
-
-func (r *relationship) Type() DependencyType {
-	return r.kind
-}
