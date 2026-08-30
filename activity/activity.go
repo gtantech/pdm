e7848803311a4e14e5db3fd679dd0392e133cbc0
gtantech <tangabrielkl@gmail.com>
2026-08-30 14:04:10 -0400

    Added duration field

--- activity/activity.go
+++ activity/activity.go
@@ -1,22 +1,31 @@
 package activity
 
 import (
+	"time"
+
 	"github.com/gtantech/toposort/graph/vertex"
 )
 
 type Activity interface {
 	vertex.Vertex[Activity]
+	Duration() time.Duration
 	DisplayName() string
 }
 
 type activity struct {
 	vertex.Vertex[Activity]
-	name string
+	name     string
+	duration time.Duration
 }
 
 // DisplayName implements [Activity].
 func (a *activity) DisplayName() string {
 	return a.name
+}
+
+// Duration implements [Activity].
+func (a *activity) Duration() time.Duration {
+	return a.duration
 }
 
 var _ vertex.Vertex[Activity] = (*activity)(nil) //ensures *activity implements vertex.Vertex[Activity] at compile time
