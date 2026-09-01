e90e9428f309b6467b4b421bef5925078769b926
gtantech <tangabrielkl@gmail.com>
2026-08-31 20:04:20 -0400

    Added timestamp test

---
+++ activity/timestamp/timestamp_test.go
@@ -0,0 +1,22 @@
+package timestamp
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gtantech/pdm/interval"
+)
+
+func TestNew(t *testing.T) {
+	early := interval.New(time.Duration(0), time.Duration(1))
+	late := interval.New(time.Duration(0), time.Duration(1))
+	ts := New(early, late)
+
+	if got, want := ts.Early(), early; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := ts.Late(), late; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
