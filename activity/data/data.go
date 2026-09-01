9514cc76af71702fc1aad240354bd7b394108429
gtantech <tangabrielkl@gmail.com>
2026-08-31 23:33:34 -0400

    Added data

---
+++ activity/data/data.go
@@ -0,0 +1,19 @@
+package data
+
+import "time"
+
+type Data interface {
+	Duration() time.Duration
+}
+
+type data struct {
+	duration time.Duration
+}
+
+func New(duration time.Duration) *data {
+	return &data{duration: duration}
+}
+
+func (d *data) Duration() time.Duration {
+	return d.duration
+}
