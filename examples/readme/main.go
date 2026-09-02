be26f13ec1587e23d2e129c677bed1eeb7c8335b
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-01 22:57:42 -0400

    Added examples (#15)
    * Added table of dependency example
    
    * Migrated changes to example
    
    * Added go mod file to exclude example directory from tests
    
    * Moved
    
    * Added comments

---
+++ examples/readme/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gtantech/pdm"
+	"github.com/gtantech/pdm/activity"
+	"github.com/gtantech/pdm/dependency"
+	"github.com/gtantech/pdm/enums"
+)
+
+type Attributes struct {
+	activity.Data
+	Name string
+}
+
+func main() {
+	// create activities
+	A := activity.New(Attributes{Data: activity.NewData(3 * time.Hour), Name: "A"})
+	B := activity.New(Attributes{Data: activity.NewData(4 * time.Hour), Name: "B"})
+	C := activity.New(Attributes{Data: activity.NewData(2 * time.Hour), Name: "C"})
+	D := activity.New(Attributes{Data: activity.NewData(5 * time.Hour), Name: "D"})
+	E := activity.New(Attributes{Data: activity.NewData(1 * time.Hour), Name: "E"})
+	F := activity.New(Attributes{Data: activity.NewData(2 * time.Hour), Name: "F"})
+	G := activity.New(Attributes{Data: activity.NewData(4 * time.Hour), Name: "G"})
+	H := activity.New(Attributes{Data: activity.NewData(3 * time.Hour), Name: "H"})
+
+	// add activities to the table of dependencies
+	tableOfDependencies := make(map[activity.Activity[Attributes]][]activity.Activity[Attributes])
+	tableOfDependencies[A] = []activity.Activity[Attributes]{}     // A has no dependencies
+	tableOfDependencies[B] = []activity.Activity[Attributes]{A}    // B depends on A
+	tableOfDependencies[C] = []activity.Activity[Attributes]{A}    // C depends on A
+	tableOfDependencies[D] = []activity.Activity[Attributes]{B}    //		.
+	tableOfDependencies[E] = []activity.Activity[Attributes]{C}    //		.
+	tableOfDependencies[F] = []activity.Activity[Attributes]{C}    //		.
+	tableOfDependencies[G] = []activity.Activity[Attributes]{D, E} // G depends on D and E
+	tableOfDependencies[H] = []activity.Activity[Attributes]{F, G} // H depends on F and G
+
+	// add values from table of dependencies into pdm
+	project := pdm.New[Attributes]()
+
+	for key := range tableOfDependencies {
+		successor := key
+		for _, predecessor := range tableOfDependencies[successor] {
+			project.AddDependency(predecessor, successor, dependency.New(enums.FS))
+		}
+	}
+
+	// update the early/late start/finish of each activity
+	project.UpdateActivityTimestamps()
+
+	// print the early/late start/finish of each activity
+	for key := range tableOfDependencies {
+		fmt.Printf("Activity %v:\n", key.Data().Name)
+		fmt.Printf("Early Start:%-5v \tEarly Finish:%-5v\n", key.Timestamps().Early().Start(), key.Timestamps().Early().Finish())
+		fmt.Printf("Late Start:%-5v \tLate Finish:%-5v\n\n", key.Timestamps().Late().Start(), key.Timestamps().Late().Finish())
+	}
+
+}
