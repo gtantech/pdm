66074d6a4d15e0400e452e1136ed741f29ee757f
gtantech <tangabrielkl@gmail.com>
2026-09-01 22:31:53 -0400

    Migrated changes to example

--- examples/dependency-table/main.go
+++ examples/dependency-table/main.go
@@ -6,37 +6,36 @@ import (
 
 	"github.com/gtantech/pdm"
 	"github.com/gtantech/pdm/activity"
-	"github.com/gtantech/pdm/activity/data"
 	"github.com/gtantech/pdm/dependency"
 	"github.com/gtantech/pdm/enums"
 )
 
-type Data struct {
-	data.ActivityData
+type Attributes struct {
+	activity.Data
 	Name string
 }
 
 func main() {
-	A := activity.New(Data{ActivityData: data.New(3 * time.Hour), Name: "A"})
-	B := activity.New(Data{ActivityData: data.New(4 * time.Hour), Name: "B"})
-	C := activity.New(Data{ActivityData: data.New(2 * time.Hour), Name: "C"})
-	D := activity.New(Data{ActivityData: data.New(5 * time.Hour), Name: "D"})
-	E := activity.New(Data{ActivityData: data.New(1 * time.Hour), Name: "E"})
-	F := activity.New(Data{ActivityData: data.New(2 * time.Hour), Name: "F"})
-	G := activity.New(Data{ActivityData: data.New(4 * time.Hour), Name: "G"})
-	H := activity.New(Data{ActivityData: data.New(3 * time.Hour), Name: "H"})
+	A := activity.New(Attributes{Data: activity.NewData(3 * time.Hour), Name: "A"})
+	B := activity.New(Attributes{Data: activity.NewData(4 * time.Hour), Name: "B"})
+	C := activity.New(Attributes{Data: activity.NewData(2 * time.Hour), Name: "C"})
+	D := activity.New(Attributes{Data: activity.NewData(5 * time.Hour), Name: "D"})
+	E := activity.New(Attributes{Data: activity.NewData(1 * time.Hour), Name: "E"})
+	F := activity.New(Attributes{Data: activity.NewData(2 * time.Hour), Name: "F"})
+	G := activity.New(Attributes{Data: activity.NewData(4 * time.Hour), Name: "G"})
+	H := activity.New(Attributes{Data: activity.NewData(3 * time.Hour), Name: "H"})
 
-	tableOfDependencies := make(map[activity.Activity[Data]][]activity.Activity[Data])
-	tableOfDependencies[A] = []activity.Activity[Data]{}     // A has no dependencies
-	tableOfDependencies[B] = []activity.Activity[Data]{A}    // B depends on A
-	tableOfDependencies[C] = []activity.Activity[Data]{A}    // C depends on A
-	tableOfDependencies[D] = []activity.Activity[Data]{B}    //		.
-	tableOfDependencies[E] = []activity.Activity[Data]{C}    //		.
-	tableOfDependencies[F] = []activity.Activity[Data]{C}    //		.
-	tableOfDependencies[G] = []activity.Activity[Data]{D, E} // G depends on D and E
-	tableOfDependencies[H] = []activity.Activity[Data]{F, G} // H depends on F and G
+	tableOfDependencies := make(map[activity.Activity[Attributes]][]activity.Activity[Attributes])
+	tableOfDependencies[A] = []activity.Activity[Attributes]{}     // A has no dependencies
+	tableOfDependencies[B] = []activity.Activity[Attributes]{A}    // B depends on A
+	tableOfDependencies[C] = []activity.Activity[Attributes]{A}    // C depends on A
+	tableOfDependencies[D] = []activity.Activity[Attributes]{B}    //		.
+	tableOfDependencies[E] = []activity.Activity[Attributes]{C}    //		.
+	tableOfDependencies[F] = []activity.Activity[Attributes]{C}    //		.
+	tableOfDependencies[G] = []activity.Activity[Attributes]{D, E} // G depends on D and E
+	tableOfDependencies[H] = []activity.Activity[Attributes]{F, G} // H depends on F and G
 
-	project := pdm.New[Data]()
+	project := pdm.New[Attributes]()
 
 	for key := range tableOfDependencies {
 		successor := key
