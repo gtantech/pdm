9b476daf3e43200368c47a2f08573829bd5356ac
gtantech <tangabrielkl@gmail.com>
2026-09-05 14:55:44 -0400

    Added Forwards/Backwards pass value to dependency

--- dependency/dependency.go
+++ dependency/dependency.go
@@ -1,11 +1,17 @@
 package dependency
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/gtantech/pdm/activity/timestamp"
 	"github.com/gtantech/pdm/enums"
 )
 
 type Dependency interface {
 	Type() enums.DependencyType
+	ForwardPassValue(predecessorTimestamp timestamp.Timestamp) time.Duration
+	BackwardPassValue(successorTimestamp timestamp.Timestamp) time.Duration
 }
 
 type relationship struct {
@@ -18,4 +24,38 @@ func New(kind enums.DependencyType) *relationship {
 
 func (r *relationship) Type() enums.DependencyType {
 	return r.kind
+}
+
+// BackwardPassValue implements [Dependency].
+func (r *relationship) BackwardPassValue(successorTimestamp timestamp.Timestamp) time.Duration {
+	interval := successorTimestamp.Late()
+	switch r.kind {
+	case enums.FF:
+		return interval.Finish()
+	case enums.FS:
+		return interval.Start()
+	case enums.SF:
+		return interval.Finish()
+	case enums.SS:
+		return interval.Start()
+	default:
+		panic(fmt.Sprintf("unknown enum used: %v", r.kind))
+	}
+}
+
+// ForwardPassValue implements [Dependency].
+func (r *relationship) ForwardPassValue(predecessorTimestamp timestamp.Timestamp) time.Duration {
+	interval := predecessorTimestamp.Early()
+	switch r.kind {
+	case enums.FF:
+		return interval.Finish()
+	case enums.FS:
+		return interval.Finish()
+	case enums.SF:
+		return interval.Start()
+	case enums.SS:
+		return interval.Start()
+	default:
+		panic(fmt.Sprintf("unknown enum used: %v", r.kind))
+	}
 }
