ec4f40724f8122fb5a4aaaff0d595e072a5b2403
gtantech <tangabrielkl@gmail.com>
2026-09-05 14:59:38 -0400

    Added UpdateEarly/Late functions to Timestamp

--- activity/timestamp/timestamp.go
+++ activity/timestamp/timestamp.go
@@ -5,6 +5,8 @@ import "github.com/gtantech/pdm/interval"
 type Timestamp interface {
 	Early() interval.Interval
 	Late() interval.Interval
+	UpdateEarly(interval interval.Interval)
+	UpdateLate(interval interval.Interval)
 }
 
 type timestamp struct {
@@ -24,6 +26,16 @@ func (d *timestamp) Early() interval.Interval {
 // Late implements [Timestamp].
 func (d *timestamp) Late() interval.Interval {
 	return d.late
+}
+
+// UpdateEarly implements [Activity].
+func (d *timestamp) UpdateEarly(interval interval.Interval) {
+	d.early = interval
+}
+
+// UpdateLate implements [Activity].
+func (d *timestamp) UpdateLate(interval interval.Interval) {
+	d.late = interval
 }
 
 var _ Timestamp = (*timestamp)(nil) //ensures duration implements Duration at compile time
