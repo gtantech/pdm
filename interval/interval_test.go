808cda1a4a8892a11ddf6644215e3884236160d2
gtantech <tangabrielkl@gmail.com>
2026-09-05 14:57:40 -0400

    Added interval creation from start/finish values and duration

--- interval/interval_test.go
+++ interval/interval_test.go
@@ -18,3 +18,33 @@ func TestNew(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestFromStart(t *testing.T) {
+	start := time.Duration(1 * time.Minute)
+	duration := time.Duration(3 * time.Minute)
+	finish := time.Duration(4 * time.Minute)
+	i := FromStart(start, duration)
+
+	if got, want := i.Start(), start; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	if got, want := i.Finish(), finish; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestFromFinish(t *testing.T) {
+	start := time.Duration(1 * time.Minute)
+	duration := time.Duration(3 * time.Minute)
+	finish := time.Duration(4 * time.Minute)
+	i := FromFinish(finish, duration)
+
+	if got, want := i.Start(), start; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	if got, want := i.Finish(), finish; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
