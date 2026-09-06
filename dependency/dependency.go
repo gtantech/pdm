50a27f9a145482a9b81c4da7582b014faaad3052
gtantech <tangabrielkl@gmail.com>
2026-09-06 19:47:14 -0400

    Added comments to dependency pkg

--- dependency/dependency.go
+++ dependency/dependency.go
@@ -17,23 +17,32 @@ type dependency[D activity.Data] struct {
 	dependsVia  relationship.Relationship
 }
 
-// DependsVia implements [Dependency].
+// DependsVia implements [Dependency]. Returns the logical relationship (FS, SS, SF, FF) of this dependency.
+//
+// Added in pdm v1.0.0.
 func (d *dependency[D]) DependsVia() relationship.Relationship {
 	return d.dependsVia
 }
 
-// Predecessor implements [Dependency].
+// Predecessor implements [Dependency]. Returns the precedessor [activity.Activity] of this dependency.
+//
+// Added in pdm v1.0.0.
 func (d *dependency[D]) Predecessor() activity.Activity[D] {
 	return d.predecessor
 }
 
-// Successor implements [Dependency].
+// Successor implements [Dependency]. Returns the Successor [activity.Activity] of this dependency.
+//
+// Added in pdm v1.0.0.
 func (d *dependency[D]) Successor() activity.Activity[D] {
 	return d.successor
 }
 
 var _ Dependency[activity.Data] = (*dependency[activity.Data])(nil) //ensures ExampleStruct implements ExampleInterface at compile time
 
+// New returns a new *[dependency].
+//
+// Added in pdm v1.0.0.
 func New[D activity.Data](predecessor activity.Activity[D], successor activity.Activity[D], dependsVia relationship.Relationship) *dependency[D] {
 	return &dependency[D]{predecessor: predecessor, successor: successor, dependsVia: dependsVia}
 }
--- dependency/table/table.go
+++ dependency/table/table.go
@@ -18,28 +18,38 @@ type depedencyTable[D activity.Data] struct {
 	table map[activity.Activity[D]][]PredecessorDependency[D]
 }
 
-// GetActivities implements [DependencyTable].
+// GetActivities implements [DependencyTable]. GetActivities returns an iterator over activities in d. The iteration order is not specified and is not guaranteed to be the same from one call to the next.
+//
+// Added in pdm v1.0.0.
 func (d *depedencyTable[D]) GetActivities() func(yield func(activity.Activity[D]) bool) {
 	return maps.Keys(d.table)
 }
 
-// GetRow implements [DependencyTable].
+// GetRow implements [DependencyTable]. GetRow returns a slice of [PredecessorDependency] and bool. The boolean value returns true if a successful result is returned, otherwise false.
+//
+// Added in pdm v1.0.0.
 func (d *depedencyTable[D]) GetRow(successor activity.Activity[D]) ([]PredecessorDependency[D], bool) {
 	predecessors, ok := d.table[successor]
 	return predecessors, ok
 }
 
-// AddRow implements [DependencyTable].
+// AddRow implements [DependencyTable]. AddRow adds an activity to the table and associates a empty slice of [PredecessorDependency] with it.
+//
+// Added in pdm v1.0.0.
 func (d *depedencyTable[D]) AddRow(successor activity.Activity[D]) {
 	d.table[successor] = []PredecessorDependency[D]{}
 }
 
-// DeleteRow implements [DependencyTable].
+// DeleteRow implements [DependencyTable]. DeleteRow deletes a row from the table with the specificed successor activity.
+//
+// Added in pdm v1.0.0.
 func (d *depedencyTable[D]) DeleteRow(successor activity.Activity[D]) {
 	delete(d.table, successor)
 }
 
-// UpdatePredecessors implements [DependencyTable].
+// UpdatePredecessors implements [DependencyTable]. UpdatePredecessors replaces the predecessors in the table at the specified successor activity.
+//
+// Added in pdm v1.0.0.
 func (d *depedencyTable[D]) UpdatePredecessors(successor activity.Activity[D], predecessors []PredecessorDependency[D]) {
 	for _, a := range predecessors {
 		if _, ok := d.table[a.Predecessor()]; !ok {
@@ -49,6 +59,9 @@ func (d *depedencyTable[D]) UpdatePredecessors(successor activity.Activity[D], p
 	d.table[successor] = predecessors
 }
 
+// Returns a new *[depedencyTable]
+//
+// Added in pdm v1.0.0.
 func New[D activity.Data]() *depedencyTable[D] {
 	return &depedencyTable[D]{table: map[activity.Activity[D]][]PredecessorDependency[D]{}}
 }
