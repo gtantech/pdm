6248d9fd480b21f62cec8cb5d5171f46abf98292
gtantech <tangabrielkl@gmail.com>
2026-09-12 23:38:17 -0400

    Added project field

--- examples/app/models/activity.go
+++ examples/app/models/activity.go
@@ -18,6 +18,7 @@ type activity struct {
 	id          uuid.UUID
 	dispName    string
 	pdmActivity _activity.Activity[Activity]
+	project     Project
 }
 
 func (a *activity) ID() uuid.UUID {
@@ -31,7 +32,7 @@ func (a *activity) DisplayName() string {
 var _ Activity = (*activity)(nil) //ensures activity implements Activity at compile time
 
 func NewActivity(dispName string, duration time.Duration, project Project) *activity {
-	a := &activity{Data: _activity.NewData(duration), id: uuid.NewV7(), dispName: dispName}
+	a := &activity{Data: _activity.NewData(duration), id: uuid.NewV7(), dispName: dispName, project: project}
 	a.pdmActivity = project.AddActivity(project.AddActivity(_activity.New(Activity(a))))
 	return a
 }
